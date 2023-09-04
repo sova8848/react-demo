b6485953b056d14268574c68d1b44adb4f5158df
sova8848 <141299829+sova8848@users.noreply.github.com>
2023-09-04 13:40:25 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+// src/App.test.js
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Learn React/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders edit code message', () => {
+  const { getByText } = render(<App />);
+  const messageElement = getByText(/Edit src\/App\.js and save to reload\./i);
+  expect(messageElement).toBeInTheDocument();
+});
+
+test('renders logo', () => {
+  const { getByAltText } = render(<App />);
+  const logoElement = getByAltText(/logo/i);
+  expect(logoElement).toBeInTheDocument();
+});
+
+test('renders App component', () => {
+  const { getByTestId } = render(<App />);
+  const appElement = getByTestId('app');
+  expect(appElement).toBeInTheDocument();
+});
+
+// Additional test cases can be added to cover more aspects of your component.
+
