53cf66766e7e7d433cc73642f49d0a35c6da3278
sova8848 <141299829+sova8848@users.noreply.github.com>
2023-09-04 14:10:30 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,25 @@
-// src/App.test.js
+// Import necessary dependencies for testing
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Learn React/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Test suite for the App component
+describe('App Component', () => {
+  it('should render the logo', () => {
+    const { getByAltText } = render(<App />);
+    const logoElement = getByAltText('logo');
+    expect(logoElement).toBeInTheDocument();
+  });
 
-test('renders edit code message', () => {
-  const { getByText } = render(<App />);
-  const messageElement = getByText(/Edit src\/App\.js and save to reload\./i);
-  expect(messageElement).toBeInTheDocument();
-});
+  it('should render the "Learn React" link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText('Learn React');
+    expect(linkElement).toBeInTheDocument();
+  });
 
-test('renders logo', () => {
-  const { getByAltText } = render(<App />);
-  const logoElement = getByAltText('logo');
-  expect(logoElement).toBeInTheDocument();
-});
-
-test('renders App component', () => {
-  const { getByTestId } = render(<App />);
-  const appElement = getByTestId('app');
-  expect(appElement).toBeInTheDocument();
+  it('should render the "Edit src/App.js" text', () => {
+    const { getByText } = render(<App />);
+    const editTextElement = getByText(/Edit src\/App\.js/);
+    expect(editTextElement).toBeInTheDocument();
+  });
 });
