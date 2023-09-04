ee371bf34af7cf6bdeb3b1f0d65346703e52c766
sova8848 <141299829+sova8848@users.noreply.github.com>
2023-09-04 15:59:42 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,8 @@
-// Import necessary dependencies for testing
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// Test suite for the App component
-describe('App Component', () => {
-  it('should render the logo', () => {
-    const { getByAltText } = render(<App />);
-    const logoElement = getByAltText('logo');
-    expect(logoElement).toBeInTheDocument();
-  });
-
-  it('should render the "Learn React" link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText('Learn React');
-    expect(linkElement).toBeInTheDocument();
-  });
-
-  it('should render the "Edit src/App.js" text', () => {
-    const { getByText } = render(<App />);
-    const editTextElement = getByText(/Edit src\/App\.js/);
-    expect(editTextElement).toBeInTheDocument();
-  });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
